28b3b37ca166ad603e349d7fd9817b638a9e073e
wojtczat <wojtczat@mcmaster.ca>
2016-12-25 03:06:44 -0500

    km km
    iin

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-/*jshint esversion: 6 */
-
+/*jshint esversion:6*/
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 // Create a new component. This component should produce some HTML
 
-const App = function() {
-    return <div>Hi</div>;
-}
+const App = () => {
+  return <div>Hi!</div>;
+  }
 
-//Take this component's generated HTML and put it
-//on the page (in the DOM).
-
-React.render(App);
+  //Take this component's generated HTML and put it
+  //on the page (in the DOM).
+  ReactDOM.render(
+    <App/>,
+    document.querySelector('.container')
+  );
