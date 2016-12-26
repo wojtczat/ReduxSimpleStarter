eff87bca3f2f5dff835ebe32d9fa269bb864653d
wojtczat <wojtczat@mcmaster.ca>
2016-12-25 19:59:52 -0500

    Part 1 Update
    Updated code from Udemy course

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-/*jshint esversion:6*/
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
 // Create a new component. This component should produce some HTML
+const API_KEY = 'AIzaSyCAjL-cLgXyivtv7D37CuigreDZfDlEhkg';
 
 const App = () => {
-  return <div>Hi!</div>;
-  }
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+}
 
-  //Take this component's generated HTML and put it
-  //on the page (in the DOM).
-  ReactDOM.render(
-    <App/>,
-    document.querySelector('.container')
-  );
+
+
+//Take this component's generated HTML and put it
+//on the page (in the DOM).
+ReactDOM.render(
+    <App/>, document.querySelector('.container')
+);
