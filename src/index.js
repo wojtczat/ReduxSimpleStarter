c919852aade8b28bf41a57722e98557bba03ebae
wojtczat <wojtczat@mcmaster.ca>
2016-12-28 16:00:35 -0500

    Section 3 Lecture 34
    Code Updated to the beginning of the 3rd section of the course.
    Updated linting contents since the old linter was giving incorrect
    errors.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,55 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyCSf9aVHOjrM-Lz23KQnk6Mpl5OYTTlfdc';
 
 // Create a new component. This component should produce some HTML
-const API_KEY = 'AIzaSyCAjL-cLgXyivtv7D37CuigreDZfDlEhkg';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    /*
+      the fat arrow function is passed to debounce, debounce takes the inner
+      function (the one that was passed) and returns a new function that can
+      only be called once every 300ms
+    */
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+
     return (
-        <div>
-            <SearchBar/>
-        </div>
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo }) }
+          videos={this.state.videos}
+        />
+      </div>
     );
+  }
 }
 
-
-
-//Take this component's generated HTML and put it
-//on the page (in the DOM).
-ReactDOM.render(
-    <App/>, document.querySelector('.container')
-);
+ReactDOM.render(<App />, document.querySelector('.container'));
